f259b04d3ee90f07aea3ebb87e43f43959266243
mdbeauche <michael.beauchemin@gmail.com>
2021-10-11 10:41:47 -0700

    remove fluff, reorganize

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,5 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../components/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
@@ -7,7 +7,7 @@ export const store = configureStore({
   },
 });
 
-export type AppDispatch = typeof store.dispatch;
+export type TypedDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
